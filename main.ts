f4cd89648c9e27e9bb63616f2564aafdd218075d
Anoosha Naz <163389690+Anoosh-Naz@users.noreply.github.com>
2024-03-25 10:42:55 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt([
+    {
+        message: "Enter your first number",
+        name: "firstNumber",
+        type: "number"
+    },
+    {
+        message: "Enter your second number",
+        name: "secondNumber",
+        type: "number"
+    },
+    {
+        message: "select one operator to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multiply", "division", "modules"] 
+
+    }
+])
+if(answer.operator === "addition"){
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if(answer.operator === "multiply"){
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if(answer.operator === "division"){
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else if(answer.operator === "modules"){
+    console.log(answer.firstNumber % answer.secondNumber)
+}
+else{
+    console.log("is not valid operator")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
